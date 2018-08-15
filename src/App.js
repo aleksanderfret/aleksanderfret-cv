15b339f6ee9d99cb70f8920ce34fdcf43831d75d
Aleksander Fret <alemik@op.pl>
2018-08-15 16:39:38 +0200

    config: enabling css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
+        <h1>My personal site</h1>
       </div>
     );
   }
