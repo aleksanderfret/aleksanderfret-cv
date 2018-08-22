43dfd360dc5ba8e0de059f81c0d3a1196c8a2b0b
Aleksander Fret <alemik@op.pl>
2018-08-22 22:06:33 +0200

    remove service worker (no https)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {I18nextProvider} from 'react-i18next';
 import i18next from 'i18next';
 
@@ -60,4 +59,3 @@ ReactDOM.render(
   </I18nextProvider>,
   document.getElementById('root')
 );
-registerServiceWorker();
