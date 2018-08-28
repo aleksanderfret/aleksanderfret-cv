8062ac546a360d2c7e3e4111509890f1c2cc6d74
Aleksander Fret <alemik@op.pl>
2018-08-28 20:53:36 +0200

    side panel navigation with active class

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,25 +2,17 @@ import React from 'react';
 import { translate } from 'react-i18next';
 import NavigationItem from './NavigationItem/NavigationItem';
 import FontIcon from '../../UI/FontIcon/FontIcon';
-import * as icons from '../../UI/FontIcon/FontIconTypes/FontIconsTypes';
 import classes from './NavigationItems.scss';
-import navigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => {
   const { t } = props;
   return(
     <ul className={classes.Navigation}>
-      <NavigationItem
-          sidepanel
-          exact
-          link='/'>
-          <span>home</span>
-        </NavigationItem>
       {t('pages', {returnObjects: true}).map((page, index) => (
         <NavigationItem
           sidepanel
           key={index}
-          link={page.route}>
+          link={`/${page.route}`}>
           {props.icons &&
             <span className={classes.MenuIcon}>
               <FontIcon
