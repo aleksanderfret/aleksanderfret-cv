78b7d8d31cbec547a3f740954cb78a3e49591946
Aleksander Fret <alemik@op.pl>
2018-08-31 14:22:01 +0200

    Profile page in progress, fix few translations, create routes data object in separate file

--- src/components/PageComponents/CV/Profile/Profile.js
+++ src/components/PageComponents/CV/Profile/Profile.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import { translate } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { translate, Trans } from 'react-i18next';
+import { routes } from '../../../../data/routes.js';
+import Profession from './Profession/Profession';
 import classes from './Profile.scss';
+
+const getRoute = (advantage, lang) => {
+  for (let key in routes) {
+    if(advantage.indexOf(`${routes[key].labels[lang]}</1>`) !== -1) {
+      return routes[key].route;
+    }
+  }
+}
+
+const getAdvantage = (advantage, index, translationKey, currentLanguage) => {
+  let adventageJsx = null;
+  if (advantage.indexOf('<1>') !== -1) {
+    adventageJsx = (
+      <Trans i18nKey={translationKey}>
+        Tresc <Link to={getRoute(advantage, currentLanguage)}>link</Link>koniec
+      </Trans>
+    );
+  } else {
+    adventageJsx = advantage;
+  }
+  return <p key={index}>{adventageJsx}</p>;
+}
 
 const profile = (props) => {
   const { t } = props;
   return(
     <div className={classes.Profile}>
       <h3>{t('title')}</h3>
+        {t('professions', {returnObjects: true}).map((profession, professionIndex) => (
+          <div
+            className={classes.Profession}
+            key={professionIndex}>
+            <h4>{profession.name}</h4>
+            {profession.advantages.map((advantage, adventageIndex) =>
+              getAdvantage(advantage, adventageIndex, `professions.${professionIndex}.advantages.${adventageIndex}`, props.i18n.language)
+            )}
+          </div>
+        ))}
+
     </div>
+
   );
 };
 
---
+++ src/data/routes.js
@@ -0,0 +1,44 @@
+export const routes = {
+  profile: {
+    route: '/profile',
+    labels: {
+      en: 'profile',
+      pl: 'profil'
+    }
+  },
+  experience: {
+    route: '/experience',
+    labels: {
+      en: 'experience',
+      pl: 'doświadczenie'
+    }
+  },
+  education: {
+    route: '/education',
+    labels: {
+      en: 'education',
+      pl: 'wykształcenie'
+    }
+  },
+  skills: {
+    route: '/skills',
+    labels: {
+      en: 'skills',
+      pl: 'umiejętności'
+    }
+  },
+  portfolio: {
+    route: '/portfolio',
+    labels: {
+      en: 'portfolio',
+      pl: 'portfolio'
+    }
+  },
+  contact: {
+    route: '/contact',
+    labels: {
+      en: 'contact',
+      pl: 'kontakt'
+    }
+  }
+}
