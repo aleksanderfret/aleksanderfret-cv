6a1b1bd645a67eb2ac4dfe5ea6082ab1630711a0
Aleksander Fret <alemik@op.pl>
2018-09-17 16:48:22 +0200

    buttons: keyDown access and focus styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Button.scss';
 
-const button = (props) => {
-  const buttonClasses = [classes.Button];
-  if (classes[props.btnType]) {
-    buttonClasses.push(classes[props.btnType]);
-  }
-  if (props.isDisplayed) {
-    buttonClasses.push(classes.AlwaysDisplayed);
+class Button extends Component {
+  state = {
+    isFocused: false
   }
 
-  return (
-    <button
-      disabled={props.disabled}
-      onClick={props.clicked}
-      className={buttonClasses.join(' ')}
-      title={props.label}
-      aria-label={props.label}>{props.children}
-    </button>
-  );
-};
+  toogleButtonFocus = () => {
+    const isFocused = !this.state.isFocused;
+    this.setState({ isFocused });
+    if (isFocused) {
+      document.addEventListener('keydown', this.onPressOK);
+    } else {
+      document.removeEventListener('keydown', this.onPressOK);
+    }
+  }
 
-export default button;
+  onPressOK = (event) => {
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      this.props.clicked(event);
+    }
+  }
+
+  render() {
+    const buttonClasses = [classes.Button];
+    if (classes[this.props.btnType]) {
+      buttonClasses.push(classes[this.props.btnType]);
+    }
+    if (this.props.isDisplayed) {
+      buttonClasses.push(classes.AlwaysDisplayed);
+    }
+    return (
+      <button
+        disabled={this.props.disabled}
+        onClick={this.props.clicked}
+        className={buttonClasses.join(' ')}
+        title={this.props.label}
+        aria-label={this.props.label}
+        onFocus={this.toogleButtonFocus}
+        onBlur={this.toogleButtonFocus}>{this.props.children}
+      </button>
+    );
+  }
+}
+
+export default Button;
