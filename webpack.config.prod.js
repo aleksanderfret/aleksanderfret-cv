94c3341e4d0529c0bfa263218d907bc9540b930a
Aleksander Fret <olekfret80@gmail.com>
2020-07-31 17:58:56 +0200

    Update dependencies 31.07.2020

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,6 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
